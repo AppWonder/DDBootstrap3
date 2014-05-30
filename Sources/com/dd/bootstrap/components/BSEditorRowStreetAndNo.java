0d25f4ab2053de3dc510af7c9d4b6a02dc2cb409
Robin <rowinski@01digitalesdesign.de>
2014-05-30 15:42:46 +0200

    Clean up import statements

--- Sources/com/dd/bootstrap/components/BSEditorRowStreetAndNo.java
+++ Sources/com/dd/bootstrap/components/BSEditorRowStreetAndNo.java
@@ -1,7 +1,6 @@
 package com.dd.bootstrap.components;
 
 import com.webobjects.appserver.WOContext;
-import com.webobjects.appserver.WOResponse;
 
 public class BSEditorRowStreetAndNo extends BSComponent {
     
