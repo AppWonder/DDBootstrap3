d210e979bf59c65367314b05a1a7301ad21d6800
Robin <rowinski@01digitalesdesign.de>
2014-05-30 17:09:47 +0200

    Adds unit support to textfield

--- Sources/com/dd/bootstrap/dynamicelements/BSEditorRowTextField.java
+++ Sources/com/dd/bootstrap/dynamicelements/BSEditorRowTextField.java
@@ -17,6 +17,7 @@ import er.extensions.components._private.ERXWOTextField;
  * @binding label
  * @binding required
  * @binding helpText
+ * @binding unit
  * 
  * @author robin
  *
@@ -26,6 +27,7 @@ public class BSEditorRowTextField extends ERXWOTextField {
 	private WOAssociation _label;		// String
 	private WOAssociation _required;	// boolean
 	private WOAssociation _helpText;	// String
+	private WOAssociation _unit;		// String
 	
 	@SuppressWarnings("rawtypes")
 	public BSEditorRowTextField(String tagname, NSDictionary nsdictionary, WOElement woelement) {
@@ -36,11 +38,13 @@ public class BSEditorRowTextField extends ERXWOTextField {
 		_label = _associations.removeObjectForKey("label");
 		_required = _associations.removeObjectForKey("required");
 		_helpText = _associations.removeObjectForKey("helpText");
+		_unit = _associations.removeObjectForKey("unit");
 	}
 	
 	@Override
 	public void appendToResponse(WOResponse response, WOContext context) {
 		BSDynamicElement.InjectCSSAndJS(response, context);
+		boolean hasUnit = (_unit != null && _unit.valueInComponent(context.component()) != null);
 		
 		response.appendContentString("<div class=\"form-group\"><label class=\"col-sm-2 control-label\">");
 		if (_label != null && _label.valueInComponent(context.component()) != null) {
@@ -49,8 +53,21 @@ public class BSEditorRowTextField extends ERXWOTextField {
 				response.appendContentString("*");
 			}
 		}
+		
 		response.appendContentString("</label><div class=\"col-sm-10\">");
+		
+		if (hasUnit) {
+			response.appendContentString("<div class=\"input-group\">");
+		}
+		
 		super.appendToResponse(response, context);
+		
+		if (hasUnit) {
+			response.appendContentString("<span class=\"input-group-addon\">");
+			response.appendContentHTMLString((String) _unit.valueInComponent(context.component()));
+			response.appendContentString("</span></div>");
+		}
+		
 		if (_helpText != null && _helpText.valueInComponent(context.component()) != null) {
 			response.appendContentString("<span class=\"help-block\">");
 			response.appendContentString((String) _helpText.valueInComponent(context.component()));
