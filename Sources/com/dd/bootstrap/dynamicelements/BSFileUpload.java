b482c707768de4796d2812ba32003a8fecff45c5
Robin <rowinski@01digitalesdesign.de>
2014-08-22 14:49:08 +0200

    - Adds support for buttonValue, default value is "Browse ..."

--- Sources/com/dd/bootstrap/dynamicelements/BSFileUpload.java
+++ Sources/com/dd/bootstrap/dynamicelements/BSFileUpload.java
@@ -9,17 +9,27 @@ import com.webobjects.appserver._private.WOFileUpload;
 import com.webobjects.foundation.NSDictionary;
 
 public class BSFileUpload extends WOFileUpload {
+	
+	WOAssociation _buttonValue;
 
 	public BSFileUpload(String aName, NSDictionary<String, WOAssociation> someAssociations, WOElement template) {
 		super(aName, someAssociations, template);
 		BSDynamicElementsHelper.AppendCSS(_associations, this);
+		
+		_buttonValue = _associations.removeObjectForKey("buttonValue");
 	}
 	
 	@Override
 	public void appendToResponse(WOResponse aResponse, WOContext aContext) {
 		BSDynamicElement.InjectCSSAndJS(aResponse, aContext);
+		String buttonValue = stringValueInContext(_buttonValue, aContext);
 		
-		aResponse.appendContentString("<span class=\"btn btn-default btn-file\"> Browse ...");
+		aResponse.appendContentString("<span class=\"btn btn-default btn-file\"> ");
+		if (buttonValue != null && buttonValue.length() != 0) {
+			aResponse.appendContentHTMLString(buttonValue);
+		} else {
+			aResponse.appendContentString("Browse ...");
+		}
 		super.appendToResponse(aResponse, aContext);
 		aResponse.appendContentString("</span>");
 	}
