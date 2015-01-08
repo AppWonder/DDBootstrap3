d5d599192c9687ff098846734b1c830d6705fc41
Robin <rowinski@01digitalesdesign.de>
2015-01-08 19:34:44 +0100

    - Adds icon support for collapsible editor part

--- Sources/com/dd/bootstrap/components/BSEditorPartCollapsible.java
+++ Sources/com/dd/bootstrap/components/BSEditorPartCollapsible.java
@@ -1,5 +1,7 @@
 package com.dd.bootstrap.components;
 
+import org.apache.commons.lang.StringUtils;
+
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WOResponse;
 
@@ -16,6 +18,16 @@ public class BSEditorPartCollapsible extends BSComponent {
 	
 	public String collapsibleId() {
 		return valueForStringBinding("collapsibleId", "collapsibleId_NULL_");
+	}
+
+	public String iconCode() {
+		String iconName = stringValueForBinding("icon", null);
+		if (StringUtils.isNotEmpty(iconName)) {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<i class=\"").append(iconName).append("\"></i>");
+			return sb.toString();
+		}
+		return null;
 	}
 	
 }
