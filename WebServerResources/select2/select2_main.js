0e9c66b4888bb537a0a1f06b745e7c23d7dfc16b
Robin <rowinski@01digitalesdesign.de>
2015-01-12 15:47:44 +0100

    - Adds preview-image support for Select2

--- WebServerResources/select2/select2_main.js
+++ WebServerResources/select2/select2_main.js
@@ -1,8 +1,34 @@
 jQuery(document).ready(function() {
+
+	function format(state) {
+		if (state && state.element && state.element.length) {
+			var select = state.element[0].parentNode;
+			var formatType = select.getAttribute('data-format-type');
+			if (formatType == 'image') {
+				console.log(state);
+				var formatVar = select.getAttribute('data-format-var');
+				var urls = window[formatVar];
+				return formatImage(state, urls);
+			}
+		}
+		return state.text;
+	}
+	
+	function formatImage(state, urls) {
+		if (state.id == 'WONoSelectionString') {
+			return state.text;
+		}
+		var index = parseInt(state.id);
+		return '<img src="' + urls[index] + '" width="50" /> ' + state.text;
+	}
+
 	jQuery('select').each(function() {
 		if (this.options.length > 5 || this.getAttribute('multiple') != null) {
 			jQuery(this).select2({
-				width: '100%'
+				width: '100%',
+				formatResult: format,
+				formatSelection: format,
+				escapeMarkup: function(m) { return m; }
 			});
 		}
 	});
